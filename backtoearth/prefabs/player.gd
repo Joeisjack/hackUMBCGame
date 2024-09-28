32e75cbf21b83438f707ea97d693aa1d0cbc0510
Joeisjack <adammangoroban@gmail.com>
2024-09-28 16:38:40 -0400

    Coyote Time for real real

--- backtoearth/prefabs/player.gd
+++ backtoearth/prefabs/player.gd
@@ -3,23 +3,30 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400
+const COYOTE_TIME = 0.08
 
 var ableToJump = false
 
 func _physics_process(delta: float) -> void:
 	# Coyote Time
-	if $Timer.time_left > 0 and is_on_floor():
+	print($Timer.time_left)
+	if is_on_floor():
 		$Timer.stop()
-	elif $Timer.is_stopped() and not is_on_floor():
+	elif $Timer.is_stopped():
 		$Timer.start()
 	
+	if $Timer.is_stopped() or $Timer.time_left > $Timer.wait_time - COYOTE_TIME:
+		# if timer is running
+		ableToJump = true
+	else:
+		ableToJump = false
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Up") and (is_on_floor() or $Timer.time_left > 0):
+	if Input.get_axis("Up", "Down") and ableToJump:
 		velocity.y = JUMP_VELOCITY
 	
 	# Get the input direction and handle the movement/deceleration.
@@ -31,3 +38,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	$Timer.stop()
